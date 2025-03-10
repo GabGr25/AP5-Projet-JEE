2b48a420b4dc41d9b9853ca9019069b9ae44ef3b
Gabriel Gressier <gabfreestyle@gmail.com>
2025-03-10 18:18:29 +0100

    Fix: classes from old project removed

--- projetJEE-core/src/main/java/junia/projetJEE/core/dao/MovieDAO.java
+++
@@ -1,6 +0,0 @@
-package junia.projetJEE.core.dao;
-
-import junia.projetJEE.core.entity.Movie;
-
-public interface MovieDAO extends GenericDAO<Movie>{
-}
--- projetJEE-core/src/main/java/junia/projetJEE/core/dao/PhaseDAO.java
+++
@@ -1,6 +0,0 @@
-package junia.projetJEE.core.dao;
-
-import junia.projetJEE.core.entity.Phase;
-
-public interface PhaseDAO extends GenericDAO<Phase> {
-}
