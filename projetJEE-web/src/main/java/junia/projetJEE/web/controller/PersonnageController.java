db330f594d79c1b64dd38d0c3fee5d9e332085f5
Gabriel Gressier <gabriel_gressier@ens.univ-artois.fr>
2025-03-10 22:16:45 +0100

    Feat: First version of PersonnageController finished, it works

--- projetJEE-web/src/main/java/junia/projetJEE/web/controller/PersonnageController.java
+++ projetJEE-web/src/main/java/junia/projetJEE/web/controller/PersonnageController.java
@@ -1,11 +1,11 @@
 package junia.projetJEE.web.controller;
 
 import junia.projetJEE.core.entity.Personnage;
+import junia.projetJEE.core.service.IdGeneratorService;
 import junia.projetJEE.core.service.PersonnageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -13,23 +13,40 @@ import java.time.LocalDate;
 @RequestMapping("/personnages")
 public class PersonnageController {
 
-    private PersonnageService personnageService;
+    private final PersonnageService personnageService;
+    private final IdGeneratorService idGeneratorService;
 
-    public PersonnageController(PersonnageService personnageService) {
+    public PersonnageController(PersonnageService personnageService, IdGeneratorService idGeneratorService) {
         this.personnageService = personnageService;
+        this.idGeneratorService = idGeneratorService;
     }
 
     @GetMapping({"/", ""})
     public String getPersonnage(ModelMap modelMap) {
+        long personnageId = idGeneratorService.generateId(personnageService);
+        Personnage personnage = personnageService.findOne(personnageId);
+        modelMap.addAttribute("personnage", personnage);
         return "Personnage";
     }
 
-    @GetMapping("/today")
-    public Personnage personnageOfTheDay(){
-        long sizePersonnages = personnageService.countAll();
-        int jourDuMois = LocalDate.now().getDayOfMonth();
-        long personnageId = (jourDuMois <= sizePersonnages) ? jourDuMois : (jourDuMois % sizePersonnages) + 1;
-        return personnageService.findOne(personnageId);
+    @PostMapping("/validator")
+    public String validatePersonnage(@RequestParam("Name")String name, ModelMap modelMap) {
+        long personnageId = idGeneratorService.generateId(personnageService);
+        Personnage personnage = personnageService.findOne(personnageId);
+        modelMap.addAttribute("personnage", personnage);
+        if(personnage.getNom().equalsIgnoreCase(name)) {
+            return "redirect:/personnages/win";
+        }
+
+        modelMap.addAttribute("errorMessage", "Ce n'est pas le bon nom ... Essayer encore !");
+        return "Personnage";
     }
 
+    @GetMapping("/win")
+    public String win(ModelMap modelMap) {
+        long personnageId = idGeneratorService.generateId(personnageService);
+        Personnage personnage = personnageService.findOne(personnageId);
+        modelMap.addAttribute("personnage", personnage);
+        return "PersonnageWin";
+    }
 }
