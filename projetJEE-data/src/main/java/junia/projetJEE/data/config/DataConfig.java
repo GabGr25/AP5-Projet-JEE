775eb49f41f53a30500b128c49aa4fc0e4846c22
Gabriel Gressier <gabfreestyle@gmail.com>
2025-03-27 18:59:21 +0100

    Feat: Change to Host the DB of the app

--- projetJEE-data/src/main/java/junia/projetJEE/data/config/DataConfig.java
+++ projetJEE-data/src/main/java/junia/projetJEE/data/config/DataConfig.java
@@ -1,10 +1,20 @@
 package junia.projetJEE.data.config;
 
 import junia.projetJEE.core.config.CoreConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 @Import(CoreConfig.class)
 public class DataConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource(".env"));
+        return configurer;
+    }
 }
